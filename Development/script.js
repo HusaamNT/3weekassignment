cc6daa33504f573c9754bbeec3b28d2571fb5511
Husaam NT <husnaeem@gmail.com>
2022-10-15 12:42:46 +0100

    added comments

--- Development/script.js
+++ Development/script.js
@@ -11,19 +11,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   function generatePassword(){
-    let passwordLength = prompt("Enter the number of characters you would like your password to be:")
-    if (passwordLength < 8 || 128 < passwordLength){
+    let passwordLength = prompt("Enter the number of characters you would like your password to be:") //gives the user a prompt window so they can choose a length for theie password
+    if (passwordLength < 8 || 128 < passwordLength){ //this makes the password between 8and 128 characters long
       window.alert("That character length isn't allowed")
     }
-    let upperCase = confirm("Would you like to include upper case letters in your password?")
+    let upperCase = confirm("Would you like to include upper case letters in your password?") //gives a confirm box for the optional characters
     if (upperCase === true){
      // console.log("hello")    this confirms if that confirm window works
     }
     let numbers = confirm("Would you like to include numbers in your password?")
     let special = confirm("Would you like to include special characters in your password?")
-    
+
     for (let i = 0; i < passwordLength; i++) {
-       let result = charsLowerCase.charAt(Math.floor(Math.random() * passwordLength));
+       let result = charsLowerCase.charAt(Math.floor(Math.random() * passwordLength)); //the random character generation for the lower case 
        
     }
     return result
