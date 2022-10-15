3bc58269b5400ba70aab9871497173a8febb5cf8
Husaam NT <husnaeem@gmail.com>
2022-10-15 12:46:50 +0100

    completed arrays

--- Development/script.js
+++ Development/script.js
@@ -2,9 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 const charsLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const charsUpperCase = [""]
-const charsNumbers = [""]
-const charsSpecial = [""]
+const charsUpperCase = ["A" , "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const charsNumbers = ["1", "2" , "3", "4", "5", "6", "7", "8", "9", "0"]
+const charsSpecial = ["!", ".", "?", ",", " ", "&", "*", "£", "$"]
 
 function writePassword() {
   var password = generatePassword();
