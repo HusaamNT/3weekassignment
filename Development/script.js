a4dae189d598fdceeb8a50a06769049eb75181cc
Husaam NT <husnaeem@gmail.com>
2022-10-15 12:27:34 +0100

    added prompts for additional character options

--- Development/script.js
+++ Development/script.js
@@ -15,6 +15,13 @@ function writePassword() {
     if (passwordLength < 8 || 128 < passwordLength){
       window.alert("That character length isn't allowed")
     }
+    let upperCase = confirm("Would you like to include upper case letters in your password?")
+    if (upperCase === true){
+     // console.log("hello")    this confirms if that confirm window works
+    }
+    let numbers = confirm("Would you like to include numbers in your password?")
+    let special = confirm("Would you like to include special characters in your password?")
+    
     for (let i = 0; i < passwordLength; i++) {
        let result = charsLowerCase.charAt(Math.floor(Math.random() * passwordLength));
        
